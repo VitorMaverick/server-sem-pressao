d17a8390457ec150c7489f1c2a92dcc773dff47e
VitorMaverick <mr.maverick1@gmail.com>
2020-10-13 00:19:04 -0300

    commit database banco-online

--- knexfile.ts
+++ knexfile.ts
@@ -2,12 +2,12 @@ import path from 'path';
 
 module.exports = {
     client: 'mysql2',
-  connection: {
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'myapp_test'
-  },
+    connection: {
+        host : 'us-cdbr-east-02.cleardb.com',
+        user : 'b4fc740d798815',
+        password : '91cced2d',
+        database : 'heroku_9e4600faf962f49'
+      },
 
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -4,10 +4,10 @@ import path from 'path';
 const db = knex({
   client: 'mysql2',
   connection: {
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'myapp_test'
+    host : 'us-cdbr-east-02.cleardb.com',
+    user : 'b4fc740d798815',
+    password : '91cced2d',
+    database : 'heroku_9e4600faf962f49'
   },
   useNullAsDefault: true,
 });
